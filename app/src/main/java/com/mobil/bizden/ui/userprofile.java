aee23648ef4ff5b4e5a9357c6c57f446677b1a27
Nagima <107074781+naagika@users.noreply.github.com>
2023-05-24 21:15:47 +0300

    userprofil toplanma alanlari ve kodlar  sildim ve konum ekledim

--- app/src/main/java/com/mobil/bizden/ui/userprofile.java
+++ app/src/main/java/com/mobil/bizden/ui/userprofile.java
@@ -12,11 +12,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.mobil.bizden.R;
 
 public class userprofile extends AppCompatActivity {
-     ListView listView;
-
-    private String[] toplanma = {"Kizilay", "Çankaya", "Beşevler", "Belediye", "Mecidiye"};
-    ListView listView2;
-    private String[] kodlar = {"128", "213", "213", "322", "232"};
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -24,15 +19,6 @@ public class userprofile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userprofile);
 
-        listView = findViewById(R.id.listView);
-        listView2 = findViewById(R.id.listView2);
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, kodlar);
-        listView2.setAdapter(adapter2);
-
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<> (this, android.R.layout.simple_list_item_1, toplanma);
-
-        listView.setAdapter(adapter);
     }
 
 
