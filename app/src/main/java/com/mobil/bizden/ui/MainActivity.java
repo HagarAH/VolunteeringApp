3ef58a5eb6bdd0305766a35bf9691066b335a2e5
Hagar Hamad <hagar.akrum@gmail.com>
2023-06-02 16:09:47 +0300

    Splash Screen

--- app/src/main/java/com/mobil/bizden/ui/MainActivity.java
+++ app/src/main/java/com/mobil/bizden/ui/MainActivity.java
@@ -1,74 +1,88 @@
 package com.mobil.bizden.ui;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
-import com.google.android.material.navigation.NavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.mobil.bizden.R;
 import com.mobil.bizden.controllers.ProfileController;
 import com.mobil.bizden.models.Profile;
 
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
-            private ImageView logoImageView;
+    private ImageView logoImageView;
+    private View rootView;
+    private Runnable animationDelayRunnable;
+    private Handler handler = new Handler();
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        logoImageView = findViewById(R.id.bizdenLogo);
+        rootView = findViewById(android.R.id.content);
+        rootView.setOnClickListener(new View.OnClickListener() {
             @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
-                logoImageView = findViewById(R.id.logo_image_view);
-                logoImageView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        FirebaseAuth mAuth= FirebaseAuth.getInstance();
-                        FirebaseUser user= mAuth.getCurrentUser();
-
-                        if (user!= null){
-                            ProfileController.ProfileCheckCallback profileCheckCallback= new ProfileController.ProfileCheckCallback() {
-                                @Override
-                                public void onProfileExists(Profile profile) {
-                                    Intent intent = new Intent(MainActivity.this, Home.class);
-                                    startActivity(intent);
-                                    finish();
-                                }
-
-                                @Override
-                                public void onProfileEmpty() {
-                                    Intent intent = new Intent(MainActivity.this, FirstLogin.class);
-                                    startActivity(intent);
-                                    finish();
-                                }
-
-                                @Override
-                                public void onProfileCheckError(Exception e) {
-                                    System.out.println(e);
-                                }
-                            };
-                            ProfileController profileController= new ProfileController();
-                            profileController.checkDocument(user.getUid(),profileCheckCallback);
-                        }
-                        else{
-                            Intent intent = new Intent(MainActivity.this, Login.class);
-                            startActivity(intent);
-                            finish();
-                        }
-
-                    }
-                });
+            public void onClick(View v) {
+                handler.removeCallbacks(animationDelayRunnable);
+                checkUserProf();
             }
+        });
+
+        animationDelayRunnable = new Runnable() {
+            @Override
+            public void run() {
+                checkUserProf();
+            }
+        };
+
+        // Start the animation delay
+        handler.postDelayed(animationDelayRunnable, 2000); // 2000 milliseconds delay
+    }
+
+    private void checkUserProf() {
+        // Stop the animation delay if it's still running
+        handler.removeCallbacks(animationDelayRunnable);
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user != null) {
+            ProfileController.ProfileCheckCallback profileCheckCallback = new ProfileController.ProfileCheckCallback() {
+                @Override
+                public void onProfileExists(Profile profile) {
+                    Intent intent = new Intent(MainActivity.this, Home.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+                @Override
+                public void onProfileEmpty() {
+                    Intent intent = new Intent(MainActivity.this, FirstLogin.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+                @Override
+                public void onProfileCheckError(Exception e) {
+                    System.out.println(e);
+                }
+            };
+            ProfileController profileController = new ProfileController();
+            profileController.checkDocument(user.getUid(), profileCheckCallback);
+        } else {
+            Intent intent = new Intent(MainActivity.this, Login.class);
+            startActivity(intent);
+            finish();
         }
-
-
+    }
+}
