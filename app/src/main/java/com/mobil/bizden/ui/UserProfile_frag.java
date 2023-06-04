8c07e87d37ebd906af1de837135aa6beedd93b55
Nagima <107074781+naagika@users.noreply.github.com>
2023-06-04 21:00:37 +0300

    userprofile firebase bagladim

--- app/src/main/java/com/mobil/bizden/ui/UserProfile_frag.java
+++ app/src/main/java/com/mobil/bizden/ui/UserProfile_frag.java
@@ -1,25 +1,38 @@
 package com.mobil.bizden.ui;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.mobil.bizden.R;
 import com.mobil.bizden.databinding.FragmentResetPassBinding;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.w3c.dom.Document;
 
 
 public class UserProfile_frag extends Fragment {
 
-    private ImageView imageViewsettings;
+
     private Button button;
 
     private FragmentResetPassBinding binding;
@@ -45,8 +58,82 @@ public class UserProfile_frag extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        button = view.findViewById(R.id.updateButton);
-        imageViewsettings = view.findViewById(R.id.imageViewsettings);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String uid = user.getUid(); // Kullanıcının uid'si
+
+            DocumentReference docRef = db.collection("profiles").document(uid);
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@androidx.annotation.NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()){
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()){
+                            String tcId = document.getString("tcId");
+                            EditText edittextTC = view.findViewById(R.id.edittextTC);
+                            edittextTC.setText(tcId);
+
+                            String telephone = document.getString("telephone");
+                            EditText edittextTelephone = view.findViewById(R.id.edittextTelephone);
+                            edittextTelephone.setText(telephone);
+
+                            String date = document.getString("birthDate");
+                            EditText editTextDate = view.findViewById(R.id.editTextDate);
+                            editTextDate.setText(date);
+
+                            String firstName = document.getString("firstName");
+                            String lastName = document.getString("lastName");
+
+                            String fullName = firstName + " " + lastName;
+
+                            TextView textView = view.findViewById(R.id.textView);
+                            textView.setText(fullName);
+
+                        } else {
+                            Log.d(TAG, " no such document  ");
+                        }
+                    }else {
+                        Log.d(TAG, "get failed with ", task.getException());
+                    }
+                }
+            });
+        } else {
+            // Kullanıcı giriş yapmamışsa ilgili işlemler
+        }
+        String uid = user.getUid();
+        DocumentReference userLocationDocRef = db.collection("userLocations").document(uid);
+        userLocationDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+            @Override
+            public void onComplete(@androidx.annotation.NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()){
+                        String location = document.getString("province");  // Eğer alan adı "location" ise
+                        EditText edittextLocation = view.findViewById(R.id.textViewPostcode);
+                        edittextLocation.setText(location);
+
+                        String mahalle = document.getString("district");
+                        String adres = document.getString("address");
+                        String fulladress = mahalle + ", " + adres;
+                        EditText adrestext = view.findViewById(R.id.textViewcity);
+                        adrestext.setText(fulladress);
+                    } else {
+                        Log.d(TAG, " no such document ");
+                    }
+                }else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+
+
+
+
+    button = view.findViewById(R.id.updateButton);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
